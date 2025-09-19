b084614011d894495b8d6a33157dcce512306bd8
JEIPEY <dima29719@gmail.com>
2025-09-19 12:01:09 +0300

    chat

--- src/theme/DocItem/TOC/Desktop/index.js
+++ src/theme/DocItem/TOC/Desktop/index.js
@@ -4,14 +4,20 @@ import {useDoc} from '@docusaurus/plugin-content-docs/client';
 import TOC from '@theme/TOC';
 export default function DocItemTOCDesktop() {
   const {toc, frontMatter} = useDoc();
-  return (
 
-    <TOC
-      toc={toc}
-      minHeadingLevel={frontMatter.toc_min_heading_level}
-      maxHeadingLevel={frontMatter.toc_max_heading_level}
-      className={ThemeClassNames.docs.docTocDesktop}
-    />
-    
+
+  return (
+    <div className='toc-main'>
+      <TOC
+        toc={toc}
+        minHeadingLevel={frontMatter.toc_min_heading_level}
+        maxHeadingLevel={frontMatter.toc_max_heading_level}
+        className={ThemeClassNames.docs.docTocDesktop}
+      />
+      <div className='toc-chat'>
+        <h2>Попробовать в чате</h2>
+        <iframe src="https://www.twitch.tv/embed/JEIPEY/chat?parent=<parent>" height="500" width="400"></iframe>
+      </div>
+    </div>
   );
 }
